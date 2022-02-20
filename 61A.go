c6f059d1c6d9aed2ca337f64eaa89d66d297f425
tmw <taomingwei1996@gmail.com>
2022-02-20 19:01:12 +0800

    finish 61A

--- 61A.go
+++ 61A.go
@@ -1,6 +1,28 @@
 package main
 
 func rotateRight(head *ListNode, k int) *ListNode {
-	
-}
+	if k <= 0{
+		return head
+	}
 
+	if head == nil || head.Next == nil{
+		return head
+	}
+
+	length := 1
+	first := head
+	var node *ListNode
+	for node=head;node.Next!=nil;node=node.Next{
+		length++
+	}
+
+	node.Next = head
+	breakNum := length - k % length
+
+	for i:=0;i<breakNum-1;i++{
+		first = first.Next
+	}
+	last := first.Next
+	first.Next = nil
+	return last
+}
